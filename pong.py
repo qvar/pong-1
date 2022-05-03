f03f419875227f8a72dfdc35be6c1b314dd7a0c7
qvar <kraushan@zohomail.in>
2022-05-03 23:05:54 +0530

    paddle added

--- pong.py
+++ pong.py
@@ -15,6 +15,24 @@ wn.setup(width=800, height=600)
 
 wn.tracer(0)
 
+# paddle A
+paddle_a = turtle.Turtle()
+paddle_a.speed(0)
+paddle_a.shape("square")
+paddle_a.color("white")
+paddle_a.shapesize(stretch_wid=5, stretch_len=1)
+paddle_a.penup()
+paddle_a.goto(-350,0)
+
+# paddle B
+paddle_a = turtle.Turtle()
+paddle_a.speed(0)
+paddle_a.shape("square")
+paddle_a.color("white")
+paddle_a.shapesize(stretch_wid=5, stretch_len=1)
+paddle_a.penup()
+paddle_a.goto(350,0)
+
 # main game loop
 while True:
     wn.update()
