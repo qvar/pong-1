0eccf1ce57769736747a664ffd6adfe92f496f3b
qvar <kraushan@zohomail.in>
2022-05-03 23:14:20 +0530

    ball added

--- pong.py
+++ pong.py
@@ -25,13 +25,21 @@ paddle_a.penup()
 paddle_a.goto(-350,0)
 
 # paddle B
-paddle_a = turtle.Turtle()
-paddle_a.speed(0)
-paddle_a.shape("square")
-paddle_a.color("white")
-paddle_a.shapesize(stretch_wid=5, stretch_len=1)
-paddle_a.penup()
-paddle_a.goto(350,0)
+paddle_b = turtle.Turtle()
+paddle_b.speed(0)
+paddle_b.shape("square")
+paddle_b.color("white")
+paddle_b.shapesize(stretch_wid=5, stretch_len=1)
+paddle_b.penup()
+paddle_b.goto(350,0)
+
+# paddle B
+ball = turtle.Turtle()
+ball.speed(0)
+ball.shape("square")
+ball.color("white")
+ball.penup()
+ball.goto(0,0)
 
 # main game loop
 while True:
